ae0f2ea39d8e47182443035f5311cbfed1fac0a8
MartinHoly00 <115696258+MartinHoly00@users.noreply.github.com>
2022-12-01 11:33:17 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,53 @@
-basic.forever(function () {
-	
-})
+/* let number1 = 2
+let number2 = 6
+
+whaleysans.showNumber(number1)
+basic.pause (1000)
+whaleysans.showNumber(number2) */
+
+
+/* let number1 = 2
+
+while (true){
+    if (input.buttonIsPressed(Button.B)) {
+        number1 = number1 + 1
+        whaleysans.showNumber(number1)
+    } if (input.buttonIsPressed(Button.A)) {
+        number1 = number1 - 1
+        whaleysans.showNumber(number1)
+    } else {
+        whaleysans.showNumber(number1)
+    }
+}  */ 
+
+/* let number1 = 2
+let makej = function() {
+    if (input.buttonIsPressed(Button.B)) {
+        number1 = number1 + 1
+        whaleysans.showNumber(number1)
+    } if (input.buttonIsPressed(Button.A)) {
+        number1 = number1 - 1
+        whaleysans.showNumber(number1)
+    } else {
+        whaleysans.showNumber(number1)
+    }
+}
+while (true){
+    makej()
+} */
+
+/* let Number1 = 2
+let makejB = function () {
+    Number1 += 1
+    whaleysans.showNumber(Number1)
+}
+
+
+let makejA = function() {
+    Number1 -= 1
+    whaleysans.showNumber(Number1)
+
+}
+
+input.onButtonPressed(Button.B, makejB)
+input.onButtonPressed(Button.A, makejA) */
